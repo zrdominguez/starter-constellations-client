11bc02b88849bf55406b7f9aef51e0fee778ab0a
Zechariah Dominguez <zechariahd@gmail.com>
2022-01-04 10:26:42 -0500

    added environment variable to file .env.prod and place it inside .gitignore

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Search from "../home/Search";
 
-function Header ({ constellations, setConstellations }) {
+function Header({ constellations, setConstellations }) {
   return (
     <div className="jumbotron jumbotron-fluid bg-info text-white rounded-0 mb-0">
       <div className="container">
-        <h1 className="display-4">Explore Constellations</h1>
+        <h1 className="display-4">I Love Constellations</h1>
         <p className="lead">Explore different constellations in the sky!</p>
         <hr />
         <Search
@@ -15,6 +15,6 @@ function Header ({ constellations, setConstellations }) {
       </div>
     </div>
   );
-};
+}
 
 export default Header;
